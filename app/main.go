52b79e5a9058fd10e7fcd29221cac865759a0f9a
rezanatha <wnrezaldi@gmail.com>
2023-11-15 00:30:35 +0700

    3rd commit test

--- app/main.go
+++ app/main.go
@@ -1,40 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"os/exec"
-	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	//fmt.Println("Logs from your program will appear here!")
-
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
-
 	cmd := exec.Command(command, args...)
+	// err := cmd.Run()
 
-	// output, err := cmd.Output()
-	// if err != nil {
-	// 	fmt.Printf("Err: %v", err)
-	// 	os.Exit(1)
-	// }
-	// fmt.Println(string(output))
+	//cmd := exec.Command("ldwds")
+	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
 
-	stderrPipe, _ := cmd.StderrPipe()
-	stdoutPipe, _ := cmd.StdoutPipe()
+	go io.Copy(os.Stdout, stdout)
+	go io.Copy(os.Stderr, stderr)
 
-	if err := cmd.Run(); err != nil {
-		if exiterror, ok := err.(*exec.ExitError); ok {
-			waitstatus := exiterror.Sys().(syscall.WaitStatus)
-			os.Exit(waitstatus.ExitStatus())
-		}
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("err: %v", err)
+		exitError, _ := err.(*exec.ExitError)
+		os.Exit(exitError.ExitCode())
 	}
 
-	go io.Copy(os.Stdout, stdoutPipe)
-	go io.Copy(os.Stderr, stderrPipe)
-
+	os.Exit(0)
 }
